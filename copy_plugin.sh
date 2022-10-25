f820cfcbda003ab26cdb7b4c631bb5367e834b4a
Sebastian Sole <scsole@outlook.co.nz>
2022-10-25 20:56:38 +1300

    Simplify messages

--- copy_plugin.sh
+++ copy_plugin.sh
@@ -12,18 +12,18 @@ else
     curdir="$1"
 fi
 
-echo
 [ -z "${USERPROFILE}" ] && USERPROFILE="$HOME"
 pluginsPath='Documents/QSC/Q-Sys Designer/Plugins'
 rm "info.lua-E" 2>/dev/null | true
 
-set -x
 cd "${USERPROFILE}"
 mkdir -p "${pluginsPath}"
 if [ -e "${curdir}/${2}.qplugx" ]; then
+    echo "Copying encrypted plugin to ${USERPROFILE}/${pluginsPath}/${2}.qplugx"
     cp "${curdir}/${2}.qplugx" \
         "${USERPROFILE}/${pluginsPath}/${2}.qplugx"
 else
+    echo "Copying plugin to ${USERPROFILE}/${pluginsPath}/${2}.qplug"
     cp "${curdir}/${2}.qplug" \
         "${USERPROFILE}/${pluginsPath}/${2}.qplug"
 fi
